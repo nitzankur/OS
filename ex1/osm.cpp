6888c7a7d6d92ad1eda29d6429b6f3c8ec1d1725
rshacham <roi.shacham@mail.huji.ac.il>
2023-03-19 12:58:05 +0200

    commit with functions

---
+++ ex1/osm.cpp
@@ -0,0 +1,127 @@
+#include "osm.h"
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define ROLLING_FACTOR 100
+#define SEC_TO_MICRO 1000000
+#define MICRO_TO_NANO 1000
+
+void empty_func() {
+
+}
+
+double osm_operation_time(unsigned int iterations) {
+    if (iterations == 0) {
+        return -1;
+    }
+
+    struct timeval iteration_start_time{};
+    struct timeval iteration_end_time{};
+
+
+
+    double seconds = 0;
+    double microseconds = 0;
+
+    // Round iterations to be a multiplication of the ROLLING FACTOR:
+    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
+
+    for (int i = 0; i < iterations; i++) {
+        if (gettimeofday (&iteration_start_time, nullptr) == -1) {
+            return -1;
+        }
+
+        for (int j = 0; j < ROLLING_FACTOR; j++) {
+            1+1;
+        }
+
+        if (gettimeofday (&iteration_end_time, nullptr) == -1) {
+            return -1;
+        }
+
+        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
+        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+    }
+
+    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / iterations * ROLLING_FACTOR;
+}
+
+double osm_function_time(unsigned int iterations) {
+    if (iterations == 0) {
+        return -1;
+    }
+
+    struct timeval iteration_start_time{};
+    struct timeval iteration_end_time{};
+
+
+
+    double seconds = 0;
+    double microseconds = 0;
+
+    // Round iterations to be a multiplication of the ROLLING FACTOR:
+    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
+
+    for (int i = 0; i < iterations; i++) {
+        if (gettimeofday (&iteration_start_time, nullptr) == -1) {
+            return -1;
+        }
+
+        for (int j = 0; j < ROLLING_FACTOR; j++) {
+            empty_func();
+        }
+
+        if (gettimeofday (&iteration_end_time, nullptr) == -1) {
+            return -1;
+        }
+
+        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
+        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+    }
+
+    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / iterations * ROLLING_FACTOR;
+
+}
+
+double osm_syscall_time(unsigned int iterations) {
+    if (iterations == 0) {
+        return -1;
+    }
+
+    struct timeval iteration_start_time{};
+    struct timeval iteration_end_time{};
+
+    double seconds = 0;
+    double microseconds = 0;
+
+    // Round iterations to be a multiplication of the ROLLING FACTOR:
+    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
+
+    for (int i = 0; i < iterations; i++) {
+        if (gettimeofday (&iteration_start_time, nullptr) == -1) {
+            return -1;
+        }
+
+        for (int j = 0; j < ROLLING_FACTOR; j++) {
+            OSM_NULLSYSCALL;
+        }
+
+        if (gettimeofday (&iteration_end_time, nullptr) == -1) {
+            return -1;
+        }
+
+        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
+        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+    }
+
+    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / iterations * ROLLING_FACTOR;
+}
+
+int main() {
+    std::cout << osm_operation_time (1000) << std::endl;
+    std::cout << osm_function_time (1000) << std::endl;
+    std::cout << osm_syscall_time (1000) << std::endl;
+    return 0;
+}
