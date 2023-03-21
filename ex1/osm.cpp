6160b8ef8e7d5e01b7ceda79300d0fda1ccec3e3
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-03-21 18:45:00 +0200

    with change

--- ex1/osm.cpp
+++ ex1/osm.cpp
@@ -11,134 +11,124 @@ void empty_func() {
 }
 
 double osm_operation_time(unsigned int iterations) {
-    if (iterations == 0) {
-        return -1;
+  if (iterations == 0) {
+      return -1;
     }
 
-    struct timeval iteration_start_time{};
-    struct timeval iteration_end_time{};
+  struct timeval iteration_start_time{};
+  struct timeval iteration_end_time{};
 
+  double seconds = 0;
+  double microseconds = 0;
 
-    double seconds = 0;
-    double microseconds = 0;
+  // Round iterations to be a multiplication of the ROLLING FACTOR:
+  iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
 
-    // Round iterations to be a multiplication of the ROLLING FACTOR:
-    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
-
-    for (int i = 0; i < iterations; i++) {
-        if (gettimeofday (&iteration_start_time, NULL) == -1) {
-            return -1;
-        }
-
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-        1+1;
-
-        if (gettimeofday (&iteration_end_time, NULL) == -1) {
-            return -1;
-        }
-
-        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
-        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+  if (gettimeofday (&iteration_start_time, NULL) == -1) {
+      return -1;
     }
 
-    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
+  for (unsigned int i = 0; i < iterations; i++) {
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+      1+1;
+    }
+
+  if (gettimeofday (&iteration_end_time, NULL) == -1) {
+      return -1;
+    }
+
+  seconds += (iteration_end_time.tv_sec - iteration_start_time.tv_sec);
+  microseconds += (iteration_end_time.tv_usec - iteration_start_time.tv_usec);
+
+  return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
 }
 
 double osm_function_time(unsigned int iterations) {
-    if (iterations == 0) {
-        return -1;
+  if (iterations == 0) {
+      return -1;
     }
 
-    struct timeval iteration_start_time{};
-    struct timeval iteration_end_time{};
+  struct timeval iteration_start_time{};
+  struct timeval iteration_end_time{};
 
-    double seconds = 0;
-    double microseconds = 0;
+  double seconds = 0;
+  double microseconds = 0;
 
-    // Round iterations to be a multiplication of the ROLLING FACTOR:
-    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
+  // Round iterations to be a multiplication of the ROLLING FACTOR:
+  iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
 
-    for (int i = 0; i < iterations; i++) {
-        if (gettimeofday (&iteration_start_time, NULL) == -1) {
-            return -1;
-        }
-
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-        empty_func();
-
-        if (gettimeofday (&iteration_end_time, NULL) == -1) {
-            return -1;
-        }
-
-        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
-        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+  if (gettimeofday (&iteration_start_time, NULL) == -1) {
+      return -1;
     }
 
-    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
+  for (unsigned int i = 0; i < iterations; i++) {
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+      empty_func();
+    }
 
+  if (gettimeofday (&iteration_end_time, NULL) == -1) {
+      return -1;
+    }
+
+  seconds += (iteration_end_time.tv_sec - iteration_start_time.tv_sec);
+  microseconds += (iteration_end_time.tv_usec - iteration_start_time.tv_usec);
+
+  return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
 }
 
 double osm_syscall_time(unsigned int iterations) {
-    if (iterations == 0) {
-        return -1;
+  if (iterations == 0) {
+      return -1;
     }
 
-    struct timeval iteration_start_time{};
-    struct timeval iteration_end_time{};
+  struct timeval iteration_start_time{};
+  struct timeval iteration_end_time{};
 
-    double seconds = 0;
-    double microseconds = 0;
+  double seconds = 0;
+  double microseconds = 0;
 
-    // Round iterations to be a multiplication of the ROLLING FACTOR:
-    iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
+  // Round iterations to be a multiplication of the ROLLING FACTOR:
+  iterations = iterations + (ROLLING_FACTOR - (iterations % ROLLING_FACTOR));
 
-    for (int i = 0; i < iterations; i++) {
-        if (gettimeofday (&iteration_start_time, NULL) == -1) {
-            return -1;
-        }
-
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-        OSM_NULLSYSCALL;
-
-
-        if (gettimeofday (&iteration_end_time, NULL) == -1) {
-            return -1;
-        }
-
-        seconds += iteration_end_time.tv_sec - iteration_start_time.tv_sec;
-        microseconds += iteration_end_time.tv_usec - iteration_start_time.tv_usec;
+  if (gettimeofday (&iteration_start_time, NULL) == -1) {
+      return -1;
     }
 
-    return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
-}
+  for (unsigned int i = 0; i < iterations; i++) {
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+      OSM_NULLSYSCALL;
+    }
 
-int main() {
-    std::cout << osm_operation_time (1000000) << std::endl;
-    std::cout << osm_function_time (1000000) << std::endl;
-    std::cout << osm_syscall_time (1000000) << std::endl;
-    return 0;
+  if (gettimeofday (&iteration_end_time, NULL) == -1) {
+      return -1;
+    }
+
+  seconds += (iteration_end_time.tv_sec - iteration_start_time.tv_sec);
+  microseconds += (iteration_end_time.tv_usec - iteration_start_time.tv_usec);
+
+  return (((seconds * SEC_TO_MICRO) + microseconds) * MICRO_TO_NANO) / (iterations * ROLLING_FACTOR);
 }
