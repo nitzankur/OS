96f20edd53fc2c17e600d2b4f8706582e5175845
nitzankur <nitzan.knoll@mail.huji.ac.il>
2023-04-18 12:40:01 +0300

    with test and readme

---
+++ ex2/cmake-build-debug/CMakeFiles/ex2.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/demo_itimer.c" "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/cmake-build-debug/CMakeFiles/ex2.dir/demo_itimer.c.o"
+  "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/demo_jmp.c" "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/cmake-build-debug/CMakeFiles/ex2.dir/demo_jmp.c.o"
+  "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/demo_singInt_handler.c" "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/cmake-build-debug/CMakeFiles/ex2.dir/demo_singInt_handler.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/uthreads.cpp" "/mnt/c/Users/97254/Documents/acadmy/OS-git/ex2/cmake-build-debug/CMakeFiles/ex2.dir/uthreads.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
