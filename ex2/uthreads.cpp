e20f6fa122c2abe65a058ad8644868629856c7e2
shacham <roi.shacham@mail.huji.ac.il>
2023-04-18 15:28:28 +0300

    function for finding min free ID

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -2,14 +2,14 @@
 // Created by mikia on 18/04/2023.
 //
 
-#include <queue>
+#include <deque>
 #include <iostream>
 
 #define QUANTUM_ERROR "the quantum_usecs should be positive"
 
 using namespace std;
 
-static enum states{
+enum states{
     READY,RUNNING,BLOCKING
 };
 class Threads{
@@ -36,7 +36,7 @@ public:
 
 };
 
-static std::queue<Threads*> ready_queue;
+static deque<Threads*> ready_queue;
 static int QUANTUM_USECS = 0;
 
 
@@ -50,6 +50,38 @@ int uthread_init(int quantum_usecs){
     return 0;
 //    Threads threads_0 = new Threads(states.READY);
 
+}
+
+int find_smallest_free_id(const deque<Threads*>& queue, int running_thread_index) {
+    bool freeNumbers[100] = {false};
+
+    for (auto thread : queue) {
+        freeNumbers[thread->get_id()] = true;
+    }
+
+    freeNumbers[running_thread_index] = true;
+
+    for (int i = 1; i < 101; i++) {
+        if (!freeNumbers[i]) {
+            return i;
+        }
+    }
+
+    cout << "Failed to find a free ID" << endl;
+    return -1;
+}
+
+int main() {
+    auto a = new Threads(READY, 1);
+    auto b = new Threads(RUNNING, 2);
+    auto c = new Threads(READY, 3);
+
+    auto dq = new deque<Threads*>;
+    dq->push_back(a);
+    dq->push_back(c);
+
+    cout << find_smallest_free_id(*dq, b->get_id()) << endl;
+    return 1;
 }
 
 
