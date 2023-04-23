0b004e5127528c756a5105ed7dc325072e99900b
nitzanknoll <nitzanknoll@mail.huji.ac.il>
2023-04-23 14:36:47 +0300

    with a lot of changes

--- ex2/uthreads.cpp
+++ ex2/uthreads.cpp
@@ -3,7 +3,9 @@
 #include "uthreads.h"
 #include <setjmp.h>
 #include <signal.h>
+#include <unistd.h>
 #include <bits/sigaction.h>
+#include <sys/time.h>
 
 #define QUANTUM_ERROR "the quantum_usecs should be positive"
 #define BLOCKED_THE_MAIN_THREAD "can't blocked the main thread"
@@ -20,7 +22,7 @@ enum states{
 };
 
 enum scheduling_states{
-    BLOCKED
+    BLOCKED_HIMSELF=1,QUANTUM_EXPIRED=26,TERMINATE=3
 };
 
 typedef unsigned long address_t;
@@ -79,17 +81,19 @@ public:
 static deque<Thread*> ready_queue(MAX_THREAD_NUM);
 static deque<Thread*> blocked_queue(MAX_THREAD_NUM);
 static Thread* running_thread;
-static int QUANTUM_USECS = SUCCESS;
-static struct sigaction sa;
+static int quantum_ticks_counter = 1;
+struct sigaction quantum_tick_action;
+static int program_quantum_usecs = 0;
+struct itimerval program_timer;
 static sigset_t set;
 
 address_t translate_address(address_t addr)
 {
     address_t ret;
-    asm volatile("xor    %%fs:SUCCESSx3SUCCESS,%SUCCESS\n"
-                 "rol    $SUCCESSx11,%SUCCESS\n"
+    asm volatile("xor    %%fs:0x30,%0\n"
+                 "rol    $0x11,%0\n"
             : "=g" (ret)
-            : "SUCCESS" (addr));
+            : "0" (addr));
     return ret;
 }
 
@@ -156,36 +160,98 @@ int sleep(){
     return SUCCESS;
 }
 
-int round_robin_scheduling(scheduling_states schedulingStates){
+void uthred_running() {
+    Thread* next_thread = ready_queue.front();
+    next_thread->change_states(RUNNING);
+    next_thread->increase_quantums();
+    running_thread = next_thread;
+//    initialize_global_timer(program_quantum_usecs);
+    //TODO: SET PROCMASK
+    ready_queue.pop_front();
+    siglongjmp(next_thread->get_env(),1);
+}
 
+
+void round_robin_scheduling(int schedulingStates) {
+    int ret_val;
     switch(schedulingStates){
-        case BLOCKED:
-            sleep();
-            uthread_resume( blocked_queue.front()->get_id())
-            Thread *current_thread = blocked_queue.front();
-            blocked_queue.pop_front();
-            ready_queue.push_back(current_thread);
+        case BLOCKED_HIMSELF:
+            uthread_sleep(running_thread->get_id());
+            delete_thread_from_ready_queue(running_thread->get_id());
+            uthread_resume( blocked_queue.front()->get_id());
+            ret_val = sigsetjmp(running_thread->get_env(), 1);
+            if(ret_val == 0){
+                uthred_running();
+            }
             break;
-            //current_time +=
+        case QUANTUM_EXPIRED:
+            delete_thread_from_ready_queue(running_thread->get_id());
+            ready_queue.push_back(running_thread);
+            ret_val = sigsetjmp(running_thread->get_env(), 1);
+            if(ret_val == 0) {
+                uthred_running();
+            }
+            break;
+        case TERMINATE:
+        default:
 
+
+            break;
+
+    }
+}
+
+int initialize_global_timer(int quantum_usecs) {
+    if (quantum_usecs < 0) {
+        std::cerr <<QUANTUM_ERROR<<std::endl;
+        return FAILURE;
+    }
+
+    program_timer.it_interval.tv_sec = quantum_usecs / MICROSECONDS_IN_SECOND;
+    program_timer.it_interval.tv_usec = quantum_usecs % MICROSECONDS_IN_SECOND;
+    // Start a virtual timer. It counts down whenever this process is executing.
+    if (setitimer(ITIMER_VIRTUAL, &program_timer, NULL)) {
+        cerr << THREAD_LIBRARY_ERROR << SET_TIMER_ERROR << endl;
+        return FAILURE;
+    }
+
+    quantum_tick_action.sa_handler = &round_robin_scheduling;
+
+    if (sigaction(SIGVTALRM, &quantum_tick_action, NULL) < 0) {
+        cerr << THREAD_LIBRARY_ERROR << SIGACTION_ERROR << endl;
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+
+int uthread_init(int quantum_usecs) {
+    if (initialize_global_timer(quantum_usecs) == FAILURE) {
+        return FAILURE;
+    }
+
+    auto threads_SUCCESS = new Thread(RUNNING, SUCCESS);
+//    running_thread = threads_SUCCESS->get_id();
+    return SUCCESS;
+}
+
+
+int uthread_resume(int tid){
+    Thread* thread = find_the_thread(tid);
+    if(thread == NULL){
+        std::cerr<<THREAD_LIBRARY_ERROR<<NUMBER_THREAD_ID_IS_NOT_VALID<<endl;
+        return FAILURE;
+    }
+    if (thread->get_states() == BLOCKING){
+        blocked_queue.pop_front();
+        thread->change_states(READY);
+        ready_queue.push_back(thread);
     }
     return SUCCESS;
-
 }
 
-int uthread_resume(){
 
-}
-
-Thread* find_the_thread(int tid){
-    for (auto thread : ready_queue) {
-       if(thread->get_id() == tid){
-           return thread;
-       }
-    }
-    return NULL;
-
-}
 
 void unblock_helper(){
 
@@ -195,32 +261,28 @@ void block_helper(){
     sigprocmask(SIG_UNBLOCK,&set,NULL);
 }
 
+
 int uthread_block(int tid){
-    //TODO: UNDERSTAND WHEN IS SHOULD BE BLOCKED AND WHEN UNBLOCKED
-    // if thread tid ==0
     if(tid==0){
-        std::cerr<<BLOCKED_THE_MAIN_THREAD<<endl;
+        std::cerr<<THREAD_LIBRARY_ERROR<<BLOCKED_THE_MAIN_THREAD<<endl;
         return FAILURE;
     }
     Thread* thread = find_the_thread(tid);
     if(tid<SUCCESS||tid > MAX_THREAD_NUM || thread == NULL){
-        std::cerr<<NUMBER_THREAD_ID_IS_NOT_VALID<<endl;
+        std::cerr<<THREAD_LIBRARY_ERROR<<NUMBER_THREAD_ID_IS_NOT_VALID<<endl;
         return FAILURE;
     }
     //if thread blocking himself
     if(tid==running_thread->get_id() ){
-        std::cerr<<NUMBER_THREAD_ID_IS_NOT_VALID<<endl;
-        return FAILURE;
+        round_robin_scheduling(BLOCKED_HIMSELF);
+        return SUCCESS;
     }
     // if thread ia already in block state
     if(thread->get_states() == BLOCKING){
         unblock_helper();
         return SUCCESS;
     }
-    //block the signal
-    else{
-
-    }
+    //TODO: understand when to do sigblock and when unblock
     sigprocmask(SIG_BLOCK,&set,NULL);
     return SUCCESS;
 }
